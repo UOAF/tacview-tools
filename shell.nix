0a58bf79d10ce69fd4a4f493578660753a343b44
Matt Kline <matt@bitbashing.io>
2025-12-18 14:10:58 -0800

    Test: Does Windows still build with Stack/Nix fun?

--- shell.nix
+++ shell.nix
@@ -1,35 +1,5 @@
-let haskellSrc = builtins.fetchTarball "https://github.com/input-output-hk/haskell.nix/archive/86e113d5ceb843885cca2e9e4e8cb199b6240e07.tar.gz";
-    haskellNix = import haskellSrc {};
-    # Import nixpkgs and pass the haskell.nix provided nixpkgsArgs
-    pkgs = import
-        # haskell.nix provides access to the nixpkgs pins which are used by our CI,
-        # hence you will be more likely to get cache hits when using these.
-        # But you can also just use your own, e.g. '<nixpkgs>'.
-        haskellNix.sources.nixpkgs
-        # These arguments passed to nixpkgs, include some patches and also
-        # the haskell.nix functionality itself as an overlay.
-        haskellNix.nixpkgsArgs;
-    project = pkgs.haskell-nix.stackProject {
-      src = pkgs.haskell-nix.cleanSourceHaskell {
-        name = "tacview";
-        src = ./.;
-      };
-      modules = [
-        { enableProfiling = true;
-          enableLibraryProfiling = true;
-          # Some fuckery with Haskell.nix - not recognizing optional build flags?
-          packages.unix.flags = { "os-string" = true; };
-          packages.directory.flags = { "os-string" = true; };
-        }
-
-      ];
-    };
-in project.shellFor {
-    exactDeps = true;
-    tools = {
-      cabal = "latest";
-      # not linking today
-      #hlint = "latest";
-      haskell-language-server = "latest";
-    };
+let pkgs = (import <nixos> {});
+in pkgs.haskell.lib.buildStackProject {
+    name = "tacview";
+    buildInputs = with pkgs; [ zlib ];
 }
