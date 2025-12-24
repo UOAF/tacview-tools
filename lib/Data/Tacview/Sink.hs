f57a527bf04f9cf0474b3f382e3e8c72dda2f930
Matt Kline <matt@bitbashing.io>
2025-12-23 19:44:17 -0800

    Reintroduce zip space leak

--- lib/Data/Tacview/Sink.hs
+++ lib/Data/Tacview/Sink.hs
@@ -1,14 +1,12 @@
 module Data.Tacview.Sink (sink, sinkZip) where
 
 import Codec.Archive.Zip
-import Codec.Archive.Zip.Internal qualified as ZI
 import Conduit
 import Control.Concurrent.Channel
 import Control.Concurrent.STM
 import Data.ByteString qualified as BS
 import Data.IORef
 import Data.List (isSuffixOf)
-import Data.Map.Strict qualified as M
 import Data.Tacview.Annotate
 import Data.Tacview (ParsedLine, showLine, zipExt, txtExt)
 import Data.Text qualified as T
@@ -60,17 +58,5 @@ writeZip z = writeZip' zn where
 
 writeZip' :: FilePath -> ConduitT () BS.ByteString (ResourceT IO) () -> IO ()
 writeZip' z src = whileIO ("while writing to " <> z) $ do
-    withBinaryFile z WriteMode $ \h -> do
-        sel <- mkEntrySelector "acmi.txt"
-        let eaCompression = M.singleton sel Deflate
-            eaEntryComment = M.empty
-            eaDeleteComment = M.empty
-            eaModTime = M.empty
-            eaExtraField = M.empty
-            eaDeleteField = M.empty
-            eaExtFileAttr = M.empty
-            ea = ZI.EditingActions{..}
-        des <- ZI.sinkEntry h sel ZI.GenericOrigin src ea
-        let cdmap = M.singleton sel des
-        ZI.writeCD h (Just "Generated with tacview-filter") cdmap
-
+    sel <- mkEntrySelector "acmi.txt"
+    createArchive z $ sinkEntry Deflate src sel
